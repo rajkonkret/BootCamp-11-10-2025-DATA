e4b02363ebbff59dcc25fec4836d8121fe50d64d
radoslawjaniak <rajkokret660@gmail.com>
2026-02-15 11:03:08 +0100

    [22 4] add chatbot_zad2.py

--- day22_15_02_2026/chatbot_zad2.py
+++ day22_15_02_2026/chatbot_zad2.py
@@ -11,7 +11,8 @@ load_dotenv()
 # print("OPENAI_API_KEY=", api_key)
 
 class ChatBot:
-    def __init__(self, model="gpt-3.5-turbo"):
+    # def __init__(self, model="gpt-3.5-turbo"):
+    def __init__(self, model="gpt-4.1"):
         """
         ChatBot w pythonie
         :param model:
@@ -39,4 +40,41 @@ class ChatBot:
             model=self.model,
             messages=self.message
         )
-        print(response)
+        # print(response)
+
+        model_message = response.choices[0].message.content
+        self.add_message("assistant", model_message)  # dodanie do pamieci konwersacji tej odpowiedzi
+        return model_message
+
+
+bot = ChatBot()
+
+if __name__ == '__main__':
+    print("Starting")
+    # print(bot.get_response("Opisz Comarch"))
+# ChatCompletion(id='chatcmpl-D9T3pKg2VSJgdwQxVtLnUV7U74n6N',
+# choices=[Choice(finish_reason='stop', index=0, logprobs=None,
+# message=ChatCompletionMessage(content='Comarch to globalna firma specjalizująca się
+# w dostarczaniu innowacyjnych rozwiązań IT dla firm z różnych branż.
+# Firma została założona w 1993 roku w Polsce i od tego czasu dynamicznie się rozwija.
+# Obecnie Comarch zatrudnia ponad 6000 pracowników i ma swoje oddziały w kilkudziesięciu krajach na całym świecie.
+# \n\nComarch oferuje szeroki zakres produktów i usług, w tym systemy ERP, CRM,
+# Business Intelligence, systemy zarządzania relacjami z klientami, systemy informatyczne dla sektora finansowego,
+# telekomunikacyjnego, zdrowotnego oraz energetycznego.
+# Firma specjalizuje się również w dostarczaniu rozwiązań z zakresu e-commerce,
+# rozwoju oprogramowania dedykowanego oraz usług chmury obliczeniowej.
+# \n\nComarch ceniony jest za wysoką jakość swoich produktów, innowacyjność,
+# elastyczne podejście do klienta oraz zaangażowanie w rozwój technologii.
+# Firma regularnie zdobywa nagrody i wyróżnienia za swoje produkty i usługi,
+# co potwierdza jej pozycję jako wiodącego dostawcy rozwiązań IT na rynku światowym.',
+# refusal=None, role='assistant', annotations=[], audio=None, function_call=None, tool_calls=None))],
+# created=1771149033, model='gpt-3.5-turbo-0125', object='chat.completion', service_tier='default',
+# system_fingerprint=None, usage=CompletionUsage(completion_tokens=325, prompt_tokens=11, total_tokens=336,
+# completion_tokens_details=CompletionTokensDetails(accepted_prediction_tokens=0,
+# audio_tokens=0, reasoning_tokens=0, rejected_prediction_tokens=0),
+# prompt_tokens_details=PromptTokensDetails(audio_tokens=0, cached_tokens=0)))
+
+# bot.get_models()
+
+print(bot.get_response("Opisz Comarch"))
+print(bot.get_response("Kto jest właścicielem?"))
