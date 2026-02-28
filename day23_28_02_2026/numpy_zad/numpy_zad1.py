66bc1e4ebdc59d0f1c2e88caa1e38f5fac267bde
radoslawjaniak <rajkokret660@gmail.com>
2026-02-28 13:18:51 +0100

    [23 39] update numpy_zad1.py

---
+++ day23_28_02_2026/numpy_zad/numpy_zad1.py
@@ -0,0 +1,27 @@
+# Fast and versatile, the NumPy vectorization, indexing,
+# and broadcasting concepts are the de-facto standards of array computing today
+
+import numpy as np
+
+# pip install numpy
+
+print(np.__version__)  # 2.4.2
+
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)  # [1 2 3 4 5]
+print(type(arr))  # <class 'numpy.ndarray'>
+
+# 0-D
+arr_0d = np.array(42)
+print(arr_0d)  # 42
+print(type(arr_0d))  # <class 'numpy.ndarray'>
+
+# 1-D
+arr_1d = np.array([1, 2, 3, 4, 5])
+print(arr_1d)  # [1 2 3 4 5]
+
+# 2-D
+arr_2d = np.array([[1, 2, 3], [4, 5, 6]])
+print(arr_2d)
+# [[1 2 3]
+#  [4 5 6]]
