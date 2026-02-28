bfd1a5ec4f05b2691ce436b0e1fe2517e067b3c9
radoslawjaniak <rajkokret660@gmail.com>
2026-02-28 11:06:58 +0100

    [23 26] update excel_wczytanie_wszystkich.py

--- day23_28_02_2026/excel_wczytanie_wszystkich.py
+++ day23_28_02_2026/excel_wczytanie_wszystkich.py
@@ -3,7 +3,7 @@ from pprint import pprint
 
 from openpyxl import load_workbook
 
-file_list = glob.glob("*.xlsx") # pokazuje liste plikow o rozszerzeniu .xlsx
+file_list = glob.glob("*.xlsx")  # pokazuje liste plikow o rozszerzeniu .xlsx
 print(file_list)
 pprint(file_list)
 # ['wyniki.xlsx',
@@ -17,3 +17,22 @@ pprint(file_list)
 #  'xlsxwriter.xlsx',
 #  'xlsxwriter5.xlsx']
 
+for file in file_list:
+    try:
+        wb = load_workbook(file)
+        ws = wb.active
+
+        value = ws['A1'].value
+        print(f"{file}: A1 = {value}")
+    except Exception as e:
+        print("Bład:", e)
+# wyniki.xlsx: A1 = Imie
+# xlsxwriter_optimized.xlsx: A1 = 0
+# Bład: File is not a zip file
+# dane_zaktualziowane.xlsx: A1 = Rank
+# openpyxl_optimized.xlsx: A1 = 0
+# tabela_przestawna.xlsx: A1 = Data
+# tabela_przestawna2.xlsx: A1 = Data
+# vgsales_formated.xlsx: A1 = Rank
+# xlsxwriter.xlsx: A1 = None
+# xlsxwriter5.xlsx: A1 = Witaj1
