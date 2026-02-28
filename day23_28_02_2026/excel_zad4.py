09140c1a7377997f701ca249d3957dc7b23d1778
radoslawjaniak <rajkokret660@gmail.com>
2026-02-28 09:16:31 +0100

    [23 9] update excel_zad4.py

--- day23_28_02_2026/excel_zad4.py
+++ day23_28_02_2026/excel_zad4.py
@@ -2,4 +2,9 @@ import xlwt
 import xlrd
 from xlutils.copy import copy
 
-book = xlrd.open_workbook('xlwt.xls')
+book = xlrd.open_workbook('xlwt.xls', formatting_info=True)  # wczytanie formatowania komórek, style, formaty liczb itd
+book = copy(book)
+
+book.get_sheet(0).write(0, 0, "zmienione")
+
+book.save('edited.xls')
