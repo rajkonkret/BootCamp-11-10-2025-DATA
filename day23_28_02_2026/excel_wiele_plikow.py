926da5211199183c5dd4d573e11ee22ace990177
radoslawjaniak <rajkokret660@gmail.com>
2026-02-28 10:59:55 +0100

    [23 24] update vgsales_formated.xlsx

--- day23_28_02_2026/excel_wiele_plikow.py
+++ day23_28_02_2026/excel_wiele_plikow.py
@@ -1,4 +1,17 @@
 from openpyxl import load_workbook
 
 wb1 = load_workbook("tabela_przestawna.xlsx")
-wb2 = load_workbook("")
+wb2 = load_workbook("vgsales_formated.xlsx")
+
+ws1 = wb1.active
+ws2 = wb2.active
+
+print(ws1)
+print(ws2)
+# <Worksheet "Dane">
+# <Worksheet "vgsales">
+
+for row in ws1.iter_rows(values_only=True):
+    ws2.append(row)
+
+wb2.save("dane_zaktualziowane.xlsx")
