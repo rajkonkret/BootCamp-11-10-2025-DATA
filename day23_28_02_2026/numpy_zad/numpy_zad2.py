3ee2b71f462c1bff4e8d390e81918d8f4bd09d5d
radoslawjaniak <rajkokret660@gmail.com>
2026-02-28 13:36:17 +0100

    [23 41] update numpy_zad2.py

---
+++ day23_28_02_2026/numpy_zad/numpy_zad2.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+a = np.array(42)
+b = np.array([1, 2, 3, 4, 5])
+c = np.array([[1, 2, 3], [4, 5, 6]])
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+# wymiary tablic
+print(a.ndim)  # 0
+print(b.ndim)  # 1
+print(c.ndim)  # 2
+print(d.ndim)  # 3
+
+arr = np.array([1, 2, 3, 4, 5], ndmin=5)
+print(arr)  # [[[[[1 2 3 4 5]]]]]
+print("Wymiar:", arr.ndim)  # Wymiar: 5
+
+# odczytaie elementów z tablicy po indeksie
+print(b)
+print(b[0])  # 1
+print(b[2])  # 3
+
+# wypisac tablice c
+# pierwszy wiersz
+# pierwszy wiersz, drugi element
+print(c)
+print("Pierwszy wiersz:", c[0])  # Pierwszy wiersz: [1 2 3]
+print("Pierwszy wiersz, drugi element:", c[0][1])  # Pierwszy wiersz, drugi element: 2
+print(c[0, 1])  # 2, podejscie numpy
+
+arr_10 = np.array(([1, 2, 3, 4, 5], [6, 7, 8, 9, 10]))
+print(arr_10)
+# [[ 1  2  3  4  5]
+#  [ 6  7  8  9 10]]
+# 4 element, wiersz 2
+print("Czwarty element, wiersz 2:", arr_10[1, 3])
+# Czwarty element, wiersz 2: 9
