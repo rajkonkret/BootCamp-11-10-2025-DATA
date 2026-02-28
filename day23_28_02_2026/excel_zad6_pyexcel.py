434a9d8cbdc2b275003eaaccd8b5e34a4017f78c
radoslawjaniak <rajkokret660@gmail.com>
2026-02-28 09:45:56 +0100

    [23 12a] update excel_zad6_pyexcel.py.py

--- day23_28_02_2026/excel_zad6_pyexcel.py
+++ day23_28_02_2026/excel_zad6_pyexcel.py
@@ -8,3 +8,16 @@ data = [
     ["Kasia", 34],
 ]
 
+sheet = pyexcel.Sheet(data)
+sheet.save_as('wyniki.xlsx')
+
+sheet = pyexcel.get_sheet(file_name='wyniki.xlsx')
+print(sheet)
+# pyexcel sheet:
+# +-------+------+
+# | Imie  | Wiek |
+# +-------+------+
+# | Tomek | 45   |
+# +-------+------+
+# | Kasia | 34   |
+# +-------+------+
