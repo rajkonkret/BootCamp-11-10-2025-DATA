a00332f9aca6edec99d4073abb93942acd0e3b70
radoslawjaniak <rajkokret660@gmail.com>
2026-03-01 11:11:50 +0100

    [24 25] update numpy_dot_matrix.py

--- day24_1_03_2026/numpy_zad/numpy_dot_matrix.py
+++ day24_1_03_2026/numpy_zad/numpy_dot_matrix.py
@@ -34,3 +34,38 @@ def generate_random_array2D(shape: Tuple[int, int]) -> PythonArray2D:
         for col in range(shape[1]):
             result[row][col] = random.random()
     return result
+
+
+if __name__ == '__main__':
+    # macierze pythonowe
+    PYTHON_ARRAY_A = generate_random_array2D(shape=(500, 500))
+    PYTHON_ARRAY_B = generate_random_array2D(shape=(500, 500))
+
+    # macierze numpy
+    NUMPY_ARRAY_A = np.array(PYTHON_ARRAY_A)
+    NUMPY_ARRAY_B = np.array(PYTHON_ARRAY_B)
+
+    start_time = time.time()
+    python_operation_result = multiply_python_arrays(x=PYTHON_ARRAY_A, y=PYTHON_ARRAY_B)
+    python_exec_time = time.time() - start_time
+    print(f"Pure Python time: {python_exec_time}")
+    print(python_operation_result[10][10])
+
+    start_time = time.time()
+    numpy_operation_result = NUMPY_ARRAY_A.dot(NUMPY_ARRAY_B)  # mnożenie macierzy
+    numpy_exec_time = time.time() - start_time
+    print(f"Numpy time: {numpy_exec_time}")
+    print(numpy_operation_result[10, 10])
+
+    print("Ratio:", python_exec_time / numpy_exec_time)
+
+    result_diff = np.sum(np.absolute(numpy_operation_result - np.array(python_operation_result)))
+    print(result_diff)  # 1.6583641126999282e-09
+    print(result_diff < 1e-5)  # True
+# Pure Python time: 11.850436925888062
+# 133.8914057103311
+# Numpy time: 0.002416849136352539
+# 133.8914057103311
+# Ratio: 4903.258853704252
+# 1.6527366142327082e-09
+# True
