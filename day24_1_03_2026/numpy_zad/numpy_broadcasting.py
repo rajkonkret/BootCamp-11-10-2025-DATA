ea5c00cfab6440e09c2b11faed0363c210a31fc5
radoslawjaniak <rajkokret660@gmail.com>
2026-03-01 10:23:53 +0100

    [24 17] update numpy_broadcasting.py

--- day24_1_03_2026/numpy_zad/numpy_broadcasting.py
+++ day24_1_03_2026/numpy_zad/numpy_broadcasting.py
@@ -5,3 +5,25 @@
 # jesli jeden z wymiarów wynoi 1, NumPy rozciąga ten wymiar by dostosowac do drugiej tablicy
 # jeśli wymiary są różne i żaden nie wynosi 1 - otrzymamy bład!!!
 import numpy as np
+
+arr = np.array([1, 2, 3])
+result = arr + 10  # -> [10, 10, 10]
+print(result)  # [11 12 13]
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([[10], [20], [30]])
+print(arr1)
+# [1 2 3]
+print(arr2)
+# [[10]
+#  [20]
+#  [30]]
+print(arr1.shape)  # (3,)
+print(arr2.shape)  # (3, 1)
+result = arr1 + arr2
+print(result)
+# [[11 12 13]
+#  [21 22 23]
+#  [31 32 33]]
+
+
