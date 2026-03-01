e08eef13bff1b6de0b97059ddb2b598341a6c396
radoslawjaniak <rajkokret660@gmail.com>
2026-03-01 10:37:46 +0100

    [24 19] update numpy_broadcasting.py

--- day24_1_03_2026/numpy_zad/numpy_broadcasting.py
+++ day24_1_03_2026/numpy_zad/numpy_broadcasting.py
@@ -39,3 +39,56 @@ result = arr1 + arr2
 print(result)
 # [[[11 22 33]
 #   [14 25 36]]]
+
+arr1 = np.ones((4, 3, 2))
+print(arr1)
+# [[[1. 1.]
+#   [1. 1.]
+#   [1. 1.]]
+#
+#  [[1. 1.]
+#   [1. 1.]
+#   [1. 1.]]
+#
+#  [[1. 1.]
+#   [1. 1.]
+#   [1. 1.]]
+#
+#  [[1. 1.]
+#   [1. 1.]
+#   [1. 1.]]]
+
+arr2 = np.array([10, 20])
+result = arr1 + arr2
+print(result)
+#  [[11. 21.]
+#   [11. 21.]
+#   [11. 21.]]]
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([10, 20])
+print(arr1.shape)  # (3,)
+print(arr2.shape)  # (2,)
+# result = arr1 + arr2
+# print(result)
+# ValueError: operands could not be broadcast together with shapes (3,) (2,)
+
+# wykorzystanie reshape() do dostosowania danych tak by był możliwy broadcasting
+arr1 = np.array([1, 2, 3]).reshape(3, 1)
+arr2 = np.array([10, 20]).reshape(1, 2)
+print(arr1.shape)  # (3, 1)
+print(arr1)
+# [[1]
+#  [2]
+#  [3]]
+print(arr2.shape)  # (1, 2)
+print(arr2)
+# [[10 20]]
+result = arr1 + arr2
+print(34 * "-")
+print(result)
+# ----------------------------------
+# [[11 21]
+#  [12 22]
+#  [13 23]]
+print(result.shape)  # (3, 2)
