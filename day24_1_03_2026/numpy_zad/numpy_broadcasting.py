44e1524ebca1552818205e1ff8d160b5f0c26dd6
radoslawjaniak <rajkokret660@gmail.com>
2026-03-01 10:15:35 +0100

    [24 16] update numpy_broadcasting.py

---
+++ day24_1_03_2026/numpy_zad/numpy_broadcasting.py
@@ -0,0 +1,7 @@
+# broadcasting
+# zamienia eleemnty tablicy wg podanego wzoru automatycznie
+# nie trzeba ręcznie wykonywac obliczenia na każdym elemencie
+# jeśli wymiary tablic są równe jest ok
+# jesli jeden z wymiarów wynoi 1, NumPy rozciąga ten wymiar by dostosowac do drugiej tablicy
+# jeśli wymiary są różne i żaden nie wynosi 1 - otrzymamy bład!!!
+import numpy as np
