04d6e0905e3ded176b4f7b078a240f8be5006218
radoslawjaniak <rajkokret660@gmail.com>
2026-03-01 14:12:52 +0100

    [24 46] update numpy_search.py

--- day24_1_03_2026/numpy_zad/numpy_search.py
+++ day24_1_03_2026/numpy_zad/numpy_search.py
@@ -23,3 +23,14 @@ x = np.searchsorted(arr, 8, side="right")
 print(x)  # 3 indeks 3
 arr = np.insert(arr, 3, 8)
 print(arr)  # [6 7 8 8 9]
+
+arr = np.array([1, 3, 5, 7])
+x = np.searchsorted(arr, [2, 4, 6])
+print(x)  # [1 2 3] - pozycja dla kazdej cyfry
+result = np.insert(arr, x, [2, 4, 6])
+print(result)  # [1 2 3 4 5 6 7]
+
+# podmiana
+arr = np.array([1, 2, np.nan, 4, np.inf, 6, -np.inf, 8])
+sanitized_arr = np.where(np.isfinite(arr), arr, 0)
+print(sanitized_arr)  # [1. 2. 0. 4. 0. 6. 0. 8.]
