2e70c223a458c726491b51715caf710fcab9948f
radoslawjaniak <rajkokret660@gmail.com>
2026-03-01 12:58:31 +0100

    [24 35] update numpy_split.py

---
+++ day24_1_03_2026/numpy_zad/numpy_split.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6])
+
+new_arr = np.array_split(arr, 3)
+print(new_arr)  # [array([1, 2]), array([3, 4]), array([5, 6])]
+
+new_arr = np.array_split(arr, 4)
+print(new_arr)  # [array([1, 2]), array([3, 4]), array([5]), array([6])]
+print(new_arr[0])  # [1 2]
+print(new_arr[1])  # [3 4]
+print(new_arr[2])  # [5]
+print(new_arr[3])  # [6]
+
+arr = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]])
+new_arr = np.array_split(arr, 3)
+print(new_arr)
+# [array([[1, 2],
+#        [3, 4]]),
+#        array([[5, 6],
+#        [7, 8]]),
+#        array([[ 9, 10],
+#        [11, 12]])]
+
+arr = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12], [13, 14], [15, 16], [17, 18]])
+new_arr = np.array_split(arr, 3)
+print(new_arr)
+# [array([[1, 2],
+#        [3, 4],
+#        [5, 6]]), array([[ 7,  8],
+#        [ 9, 10],
+#        [11, 12]]), array([[13, 14],
+#        [15, 16],
+#        [17, 18]])]
+
+arr = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12], [13, 14], [15, 16], [17, 18]])
+new_arr = np.array_split(arr, 3, axis=1)
+print(new_arr)
+# [array([[ 1],
+#        [ 3],
+#        [ 5],
+#        [ 7],
+#        [ 9],
+#        [11],
+#        [13],
+#        [15],
+#        [17]]),
+#        array([[ 2],
+#        [ 4],
+#        [ 6],
+#        [ 8],
+#        [10],
+#        [12],
+#        [14],
+#        [16],
+#        [18]]),
+#        array([], shape=(9, 0), dtype=int64)]
