a264c5e6773c79acf558d092b6b151ad56e551fb
radoslawjaniak <rajkokret660@gmail.com>
2026-03-01 09:46:42 +0100

    [24 14] update numpy_arange.py

---
+++ day24_1_03_2026/numpy_zad/numpy_arange.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+arr = np.arange(10)
+print(arr)  # [0 1 2 3 4 5 6 7 8 9]
+print(arr.dtype)  # int64
+
+arr = np.arange(5, 23)
+print(arr)  # [ 5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22]
+print(arr.dtype)  # int64
+
+arr = np.arange(0, 20, 2)  # start, stop, krok
+print(arr)  # [ 0  2  4  6  8 10 12 14 16 18]
+print(arr.dtype)  # int64
+
+arr = np.arange(10, 0, -1)
+print(arr)  # [10  9  8  7  6  5  4  3  2  1]
+
+arr = np.arange(0, 10, -1)
+print(arr)  # []
+
+arr = np.arange(0, 1, 0.1)
+print(arr)  # [0.  0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9]
+
+arr = np.arange(0, 5, dtype="float32")
+print(arr)  # [0. 1. 2. 3. 4.]
+print(arr.dtype)  # float32
+
+arr = np.arange(1, 5, dtype="complex")  # liczby zespolone
+print(arr)  # [1.+0.j 2.+0.j 3.+0.j 4.+0.j]
+print(arr.dtype)  # complex128
+
+arr = np.arange(0, 5, 0.5)
+print(arr)  # [0.  0.5 1.  1.5 2.  2.5 3.  3.5 4.  4.5]
+print(arr.dtype)  # float64
+
+arr = np.linspace(0, 1, 10)
+print(arr)
+# [0.         0.11111111 0.22222222 0.33333333 0.44444444 0.55555556
+#  0.66666667 0.77777778 0.88888889 1.        ]
+
+arr = np.linspace(0, 1, num=10, endpoint=False)
+print(arr)  # [0.  0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9]
