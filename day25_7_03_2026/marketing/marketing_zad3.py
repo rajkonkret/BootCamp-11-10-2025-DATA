80c4456c3b8c4ee3aecbf6c505c8c4dfbbc3fa54
radoslawjaniak <rajkokret660@gmail.com>
2026-03-07 14:26:58 +0100

    [25 51] update marketing_zad3.py

--- day25_7_03_2026/marketing/marketing_zad3.py
+++ day25_7_03_2026/marketing/marketing_zad3.py
@@ -47,4 +47,15 @@ plt.xticks(rotation=45)
 
 plt.show()
 
+language = df.groupby(['date_served', 'language_preferred'])['user_id'].count()
+language = pd.DataFrame(language.unstack(level=0))
+print(language.head())
 
+language.plot()
+plt.title("Dzienne preferencje językowe")
+plt.xlabel("Data")
+plt.ylabel("Użytkownicy")
+plt.legend(loc="upper right", labels=language.columns.values)
+plt.xticks(rotation=45)
+
+plt.show()
