369b94f6a5808c46865aa1a8e5bcbfb2da59c608
radoslawjaniak <rajkokret660@gmail.com>
2026-03-07 09:43:29 +0100

    [25 12a] update pandas_ex5.py

--- day25_7_03_2026/pandas_ex6.py
+++ day25_7_03_2026/pandas_ex6.py
@@ -34,3 +34,50 @@ print(df.loc[141])
 # Maxpulse    127.0
 # Calories    130.0
 # Name: 141, dtype: float64
+
+df = pd.read_csv('data.csv')
+df.fillna({"Calories": 130}, inplace=True)
+df.info()
+# <class 'pandas.DataFrame'>
+# RangeIndex: 169 entries, 0 to 168
+# Data columns (total 4 columns):
+#  #   Column    Non-Null Count  Dtype
+# ---  ------    --------------  -----
+#  0   Duration  169 non-null    int64
+#  1   Pulse     169 non-null    int64
+#  2   Maxpulse  169 non-null    int64
+#  3   Calories  169 non-null    float64
+# dtypes: float64(1), int64(3)
+# memory usage: 5.4 KB
+print(df.loc[141])
+# Duration     60.0
+# Pulse        97.0
+# Maxpulse    127.0
+# Calories    130.0
+# Name: 141, dtype: float64
+
+# superbezpieczne podejscie, zalecane pandas 3
+df = pd.read_csv('data.csv')
+df['Calories'] = df['Calories'].fillna(130)
+df.info()
+# <class 'pandas.DataFrame'>
+# RangeIndex: 169 entries, 0 to 168
+# Data columns (total 4 columns):
+#  #   Column    Non-Null Count  Dtype
+# ---  ------    --------------  -----
+#  0   Duration  169 non-null    int64
+#  1   Pulse     169 non-null    int64
+#  2   Maxpulse  169 non-null    int64
+#  3   Calories  169 non-null    float64
+# dtypes: float64(1), int64(3)
+# memory usage: 5.4 KB
+
+# od pandasa 3.0 to nie zmieni danych w oryginalnej DataFrame/Series
+df = pd.read_csv('data.csv')
+df['Calories'].fillna(130, inplace=True)
+print(df.loc[141])
+# Duration     60.0
+# Pulse        97.0
+# Maxpulse    127.0
+# Calories      NaN -> brak zmiany
+# Name: 141, dtype: float64
