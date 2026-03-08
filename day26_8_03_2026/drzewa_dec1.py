962da5b87a2ba349f489e3fc27f71c28100d5bb0
radoslawjaniak <rajkokret660@gmail.com>
2026-03-08 11:22:23 +0100

    [26 28] update drzewa_dec1.py

---
+++ day26_8_03_2026/drzewa_dec1.py
@@ -0,0 +1,50 @@
+#     # START
+#     #  |
+#     #  └── Czy wiek > 30?
+#     #        |
+#     #        ├── NIE  →  Pożyczka odrzucona
+#     #        |
+#     #        └── TAK
+#     #              |
+#     #              └── Czy zarobki > 40000?
+#     #                    |
+#     #                    ├── NIE  →  Pożyczka odrzucona
+#     #                    |
+#     #                    └── TAK  →  Pożyczka przyznana
+#
+#     #          Czy wiek > 50?
+#     #            /       \
+#     #          Tak        Nie
+#     #         /             \
+#     # Czy zarobki>60?   Czy wiek>30?
+#     #     /    \             /    \
+#     #   Tak   Nie         Tak     Nie
+#     #  (OK)  (NIE)     Czy zarobki>40?  \
+#     #                          /   \     \
+#     #                        Tak  Nie   (NIE)
+#     #
+
+def decyzja_po_ifach(wiek, zarobki):
+    # if wiek > 30 and zarobki > 40_000:
+    #     return "Pożyczka przyznana"
+    # else:
+    #     return "Pożyczka odrzucona"
+
+    if wiek > 50:
+        if zarobki > 60_000:
+            return "Pożyczka przyznana"
+        else:
+            return "Pożyczka odrzucona"
+    else:
+        if wiek > 30:
+            if zarobki > 40_000:
+                return "Pożyczka przyznana"
+            else:
+                return "Pożyczka odrzucona"
+        else:
+            return "Pożyczka odrzucona"
+
+
+print(decyzja_po_ifach(30, 40000))  # Pożyczka odrzucona
+print(decyzja_po_ifach(55, 70000))  # Pożyczka przyznana
+print(decyzja_po_ifach(25, 100000))  # Pożyczka odrzucona
