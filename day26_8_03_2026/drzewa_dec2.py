1a0b8053e5c57635798d50ce26a5e6cdba8ed95d
radoslawjaniak <rajkokret660@gmail.com>
2026-03-08 11:45:38 +0100

    [26 30] update drzewa_dec2.py

--- day26_8_03_2026/drzewa_dec2.py
+++ day26_8_03_2026/drzewa_dec2.py
@@ -12,3 +12,91 @@ dane = [
     {"wiek": 52, "zarobki": 80, "decyzja": "tak"},
     {"wiek": 60, "zarobki": 55, "decyzja": "tak"},
 ]
+
+
+# wskażnik giniego lub entropia (entropia - logarytmiczna - wolniejsze)
+# funkcja Giniego (0 - dobrze)
+def gini_index(dane):
+    total = len(dane)
+    if total == 0:
+        return 0
+
+    decyzje = [x['decyzja'] for x in dane]
+    pozytywne = decyzje.count('tak') / total
+    negatywne = decyzje.count('nie') / total
+
+    gini = 1 - (pozytywne ** 2 + negatywne ** 2)
+    return gini
+
+
+def gini_po_podziale(lewa, prawa):
+    total = len(lewa) + len(prawa)
+    w1 = len(lewa) / total
+    w2 = len(prawa) / total
+
+    return w1 * gini_index(lewa) + w2 * gini_index(prawa)
+
+
+def wypisz_grupe(nazwa, grupa):
+    print(f"\n {nazwa} (liczba {len(grupa)})")
+    print(f" tak:", sum(1 for x in grupa if x['decyzja'] == 'tak'))
+    print(f" nie:", sum(1 for x in grupa if x['decyzja'] == 'nie'))
+    print(f" Gini: {round(gini_index(grupa), 3)}")
+
+
+# dzielimy wg wiek > 50
+wiek_value = 50
+wiek_lewo = [x for x in dane if x['wiek'] > wiek_value]
+wiek_prawo = [x for x in dane if x['wiek'] <= wiek_value]
+
+# obliczmy wskażnik
+print("Gini (całośc)", round(gini_index(dane), 3))
+print("Gini po podziale (wiek > 50)", round(gini_po_podziale(wiek_lewo, wiek_prawo), 3))
+# Gini (całośc) 0.48
+# Gini po podziale (wiek > 50) 0.45 -> raczej idziemy w dobrą strone, poprawnie wybrany podział
+
+wypisz_grupe("wiek > 50", wiek_lewo)
+wypisz_grupe("wiek =< 50", wiek_prawo)
+#  wiek > 50 (liczba 4
+#  tak: 3
+#  nie: 1
+#  Gini: 0.375
+#
+#  wiek =< 50 (liczba 6
+#  tak: 3
+#  nie: 3
+#  Gini: 0.5
+
+zarobki_value = 50
+zarobki_lewo = [x for x in dane if x['zarobki'] > zarobki_value]
+zarobki_prawo = [x for x in dane if x['zarobki'] <= zarobki_value]
+
+print("Gini (całośc)", round(gini_index(dane), 3))
+print("Gini po podziale (wiek > 50)", round(gini_po_podziale(zarobki_lewo, zarobki_prawo), 3))
+# Gini (całośc) 0.48
+# Gini po podziale (wiek > 50) 0.4
+
+wypisz_grupe("zarobki > 50", zarobki_lewo)
+wypisz_grupe("zarobki =< 50", zarobki_prawo)
+
+
+# wizualizacja
+def narysuj_podzial(lewa, prawa, tytul, split_f):
+    plt.figure(figsize=(10, 4))
+    for grupa, nazwa, kolor in zip([lewa, prawa], [f"{split_f} > 50", f"{split_f} < 50"], ["green", "red"]):
+        x = [x['wiek'] for x in grupa]
+        y = [x['zarobki'] for x in grupa]
+        decyzje = [x['decyzja'] for x in grupa]
+        kolory = ['blue' if d == "tak" else "grey" for d in decyzje]
+        plt.scatter(x, y, c=kolory, label=nazwa, edgecolors=kolor, s=100, alpha=0.8)
+
+    plt.xlabel("Wiek")
+    plt.ylabel("Zarobki")
+    plt.title(f"Podział: {split_f} > 50")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+
+narysuj_podzial(wiek_lewo, wiek_prawo, "Podział po wieku", "wiek")
