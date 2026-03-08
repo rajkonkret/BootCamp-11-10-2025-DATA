31417838d22d27662aba40d85bbc05660da58c39
radoslawjaniak <rajkokret660@gmail.com>
2026-03-08 14:35:00 +0100

    [26 46] update perceptron_xor3.py

---
+++ day26_8_03_2026/perceptron_xor2_sigm_wielow.py
@@ -0,0 +1,47 @@
+# perceptron wielowarstwowy
+
+# warstwa wejściowa
+# warstwa ukryta
+# warstwa wyjściowa
+
+# Optuna/KersTuner
+import numpy as np
+
+
+# funkcja aktywacji, sigmoid
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+# dane trenigowa XOR
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([[0], [1], [1], [0]])
+
+np.random.seed(42)
+
+W_hidden = np.random.rand(2, 2)
+W_output = np.random.rand(2, 1)
+
+learning_rate = 0.5
+epochs = 1000
+
+for epoch in range(epochs):
+    # warstwa ukryta
+    hidden_input = np.dot(X, W_hidden)
+    hidden_output = sigmoid(hidden_input)
+
+    # warstwa wyjściowa
+    output_input = np.dot(hidden_output, W_output)
+    output = sigmoid(output_input)
+
+    # bład
+    error = y - output
+
+    # aktualizacja wag, backpropagation
+    d_output = error * (output * (1 - output))
+    error_hidden = d_output.dot(W_output.T)
+    d_hidden = error_hidden * (hidden_output * (1 - hidden_output))
+
+    # aktualizacja wag
+    W_output += hidden_output.T.dot(d_output) * learning_rate
+    W_hidden += X.T.dot(d_hidden) * learning_rate
