f9ade9dfe8cf017c5e736333a10c3212d349b7fc
radoslawjaniak <rajkokret660@gmail.com>
2026-03-08 12:36:12 +0100

    [26 34] update drzewa_dec4.py

---
+++ day26_8_03_2026/drzea_dec4.py
@@ -0,0 +1,12 @@
+dane = [
+    {"wiek": 55, "zarobki": 70, "decyzja": "tak"},
+    {"wiek": 53, "zarobki": 50, "decyzja": "nie"},
+    {"wiek": 47, "zarobki": 60, "decyzja": "tak"},
+    {"wiek": 40, "zarobki": 30, "decyzja": "nie"},
+    {"wiek": 35, "zarobki": 45, "decyzja": "tak"},
+    {"wiek": 28, "zarobki": 65, "decyzja": "nie"},
+    {"wiek": 31, "zarobki": 42, "decyzja": "tak"},
+    {"wiek": 29, "zarobki": 50, "decyzja": "nie"},
+    {"wiek": 52, "zarobki": 80, "decyzja": "tak"},
+    {"wiek": 60, "zarobki": 55, "decyzja": "tak"},
+]
