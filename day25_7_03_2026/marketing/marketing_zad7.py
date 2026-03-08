3de3d0fd4b2d099a594aa65601b2135533462f06
radoslawjaniak <rajkokret660@gmail.com>
2026-03-08 08:34:27 +0100

    [26 4] update marketing_zad7.py

--- day25_7_03_2026/marketing/marketing_zad7.py
+++ day25_7_03_2026/marketing/marketing_zad7.py
@@ -10,3 +10,9 @@ df['date_served'] = pd.to_datetime(df['date_served'], format="%m/%d/%y")
 print(df.head(3))
 #       user_id date_served  ... is_retained is_house_ads
 # 0  a100000029  2018-01-01  ...        True         True
+
+house_ads = df[df['subscribing_channel'] == "House Ads"]
+print(house_ads.head(3))
+
+house_ads_b = house_ads[house_ads['date_served'] < '2018-01-11']
+print(house_ads_b)
