5fe180e9cb9a1a1857398ee6cc258c221ce085a1
radoslawjaniak <rajkokret660@gmail.com>
2026-03-08 14:47:58 +0100

    [26 49] update perceptron_xor4

---
+++ day26_8_03_2026/per_xor4_relu.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+
+# ReLU - funkcja aktywacji
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+# pochodna relu
+def relu_deriavative(x):
+    return np.where(x > 0, 1, 0)
+
+
+# dane trenigowa XOR
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([[0], [1], [1], [0]])
+
+np.random.seed(42)
+
+W_hidden = np.random.rand(2, 2) * 2 - 1
+W_output = np.random.rand(2, 2) * 2 - 1
+
+learning_rate = 0.1
+epochs = 5000
+
+for epoch in range(epochs):
+    # warstwa ukryta
+    hidden_input = np.dot(X, W_hidden)
+    hidden_output = relu(hidden_input)
+
+    # warstwa wyjściowa
+    final_input = np.dot(hidden_output, W_output)
+    final_output = relu(final_input)
+
+    # bład
+    error = y - final_output
+
+    # aktualizacja wag, backpropagation
+    d_output = error * relu_deriavative(final_output)
+    error_hidden = d_output.dot(W_output.T)
+    d_hidden = error_hidden * relu_deriavative(hidden_output)
+
+    # aktualizacja wag
+    W_output += hidden_output.T.dot(d_output) * learning_rate
+    W_hidden += X.T.dot(d_hidden) * learning_rate
