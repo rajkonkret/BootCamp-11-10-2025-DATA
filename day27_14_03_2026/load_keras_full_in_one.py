d1d52f50bc4a67fdc65f017458fa860c080ab296
radoslawjaniak <rajkokret660@gmail.com>
2026-03-14 10:56:08 +0100

    [27 18] update keras_full_in_one.py

--- day27_14_03_2026/load_keras_full_in_one.py
+++ day27_14_03_2026/load_keras_full_in_one.py
@@ -32,6 +32,27 @@ X = np.array(
     ]
 )
 
+def relu(x):
+    return np.maximum(0, x)
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+# warstwa ukryta
+hidden_layer = np.dot(X, W_input_hidden) + b_hidden
+hidden_layer = relu(hidden_layer)
+
+# warstwa wyjściowa
+predictions = np.dot(hidden_layer, W_hidden_output) + b_output
+predictions = sigmoid(predictions)
+
+print('Predictions shape:', predictions.shape)
+
+predictions = (predictions > 0.5).astype(int)
+
+# sprawdzanie wyników odpowiedzi
 y_expected = np.array(
     [
         [0, 0, 0],  # and, or, xor
@@ -40,3 +61,8 @@ y_expected = np.array(
         [1, 1, 0],
     ]
 )
+
+print("Przewidywane wartości dla AND, OR, XOR\n")
+for i in range(len(X)):
+    print(f"Wejście {X[i]} => AND: {predictions[i][0]} OR: {predictions[i][1]}, XOR: {predictions[i][2]}")
+
