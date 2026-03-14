fbd5aef763ffd25ccd1a3d5642a94aa9d5df764f
radoslawjaniak <rajkokret660@gmail.com>
2026-03-14 10:30:43 +0100

    [27 11] update keras_full_in_one.py

---
+++ day27_14_03_2026/keras_full_in_one.py
@@ -0,0 +1,54 @@
+import os
+import numpy as np
+import time
+
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"  # przed importowaniem kerasa
+
+import tensorflow as tf
+from tensorflow import keras
+from keras import Input
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+print("Dostępne urządzenia:", tf.config.list_physical_devices())
+
+# dane wejściowe
+X = np.array(
+    [
+        [0, 0],
+        [0, 1],
+        [1, 0],
+        [1, 1]
+    ]
+)
+
+y = np.array(
+    [
+        [0, 0, 0],  # and, or, xor
+        [0, 1, 1],
+        [0, 1, 1],
+        [1, 1, 0],
+    ]
+)
+
+# tworzymy model
+# 2 wejścia
+# 4 neurony warstwa ukryta
+# 3 neurony warstwa wyjścia
+model = Sequential(
+    [
+        Input(shape=(2, 0)),
+        Dense(4, activation="relu"),
+        Dense(3, activation="sigmoid")
+    ]
+)
+
+# kompilacja modelu
+model.compile(optimizer="adam", loss="binary_crossantropy", metrics=['accuracy'])
+
+start_time = time.time()
+
+with tf.device("/GPU:0"):
+    model.fit(X, y, epochs=2000, verbose=1)
+
+print(f"Estimated time: {time.time() - start_time}")
