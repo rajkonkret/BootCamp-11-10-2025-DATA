ff86e77e801cffc4d0ab9531d2ecaafec7eaaee8
radoslawjaniak <rajkokret660@gmail.com>
2026-03-14 08:55:02 +0100

    [27 3] update tensorflow_zad1.py

--- day27_14_03_2026/tensorflow_zad1.py
+++ day27_14_03_2026/tensorflow_zad1.py
@@ -19,5 +19,57 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
 print("Dostępne urządzenia")
-print(tf.config.list_physical_devices()) # [PhysicalDevice(name='/physical_device:CPU:0', device_type='CPU')]
-print("Czy GPU jest dostępne:", tf.config.list_physical_devices('GPU')) # Czy GPU jest dostępne: []
+print(tf.config.list_physical_devices())  # [PhysicalDevice(name='/physical_device:CPU:0', device_type='CPU')]
+print("Czy GPU jest dostępne:", tf.config.list_physical_devices('GPU'))  # Czy GPU jest dostępne: []
+
+# dane wejściowe
+X = np.array(
+    [
+        [0, 0],
+        [0, 1],
+        [1, 0],
+        [1, 1]
+    ]
+)
+
+# dane do nauki
+y_xor = np.array([[0], [1], [1], [0]])
+y_and = np.array([[0], [0], [0], [1]])
+y_or = np.array([[0], [1], [1], [1]])
+
+
+def train_model(y_train, logic_type):
+    print(f"Uczenie modelu dla operacji: {logic_type}")
+
+    # definiujemy model
+    model = Sequential(
+        [
+            Input(shape=(2,)),
+            Dense(4, activation="relu"),  # warstwa ukryta, 4 neurony, funkcja relu
+            Dense(1, activation="sigmoid")  # warstwa wyjsciowa, sigmoid
+        ]
+    )
+
+    # kompilacja modelu
+    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=['accuracy'])
+
+    # uruchomienie nauki
+    with tf.device("/GPU:0"):
+        model.fit(X, y_train, epochs=100, verbose=1)
+
+    # testowanie modelu
+    predictions = model.predict(X)
+    predictions = (predictions > 0.5).astype(int)
+
+    print(f"Przewidywanie wyników dla operacji {logic_type}")
+    for i in range(len(X)):
+        print(f"{X[i]} {predictions[i][0]} oczekiwanie: {y_train[i][0]}")
+
+    return model
+
+
+start_time = time.time()
+
+model_and = train_model(y_and, "AND")
+
+print(f"Estimated time: {time.time() - start_time}")
