e0d6c16cda1073e279e4d731154fcd53b4c712ae
radoslawjaniak <rajkokret660@gmail.com>
2026-03-14 13:13:53 +0100

    [27 29] update rozpoznawanie_obrazu.py

--- day27_14_03_2026/rozpoznawanie_obrazu.py
+++ day27_14_03_2026/rozpoznawanie_obrazu.py
@@ -41,3 +41,61 @@ w1 = np.random.randn(784, 128) * 0.01
 b1 = np.zeros((1, 128))
 w2 = np.random.randn(128, 10) * 0.01
 b2 = np.zeros((1, 10))
+
+# parametry treningu
+lr = 0.1
+epochs = 5
+batch_size = 64
+
+# trening
+for epoch in range(epochs):
+    for i in range(0, len(x_train), batch_size):
+        x_batch = x_train[i:i + batch_size]
+        y_batch = y_train_oh[i:i + batch_size]
+
+        z1 = x_batch @ w1 + b1  # @ odpowiednik dot
+        a1 = relu(z1)
+        z2 = a1 @ w2 + b2
+        y_pred = softmax(z2)
+
+        # loss
+        loss = cross_entropy(y_pred, y_batch)
+
+        # backpropagation
+        dz2 = y_pred - y_batch
+        dw2 = a1.T @ dz2 / batch_size
+        db2 = np.sum(dz2, axis=0, keepdims=True) / batch_size
+
+        da1 = dz2 @ w2.T
+        dz1 = da1 * relu_derivative(z1)
+        dw1 = x_batch.T @ dz1 / batch_size
+        db1 = np.sum(dz1, axis=0, keepdims=True) / batch_size
+
+        # aktualizacja wag i bias
+        w1 -= lr * dw1
+        b1 -= lr * db1
+        w2 -= lr * dw2
+        b2 -= lr * db2
+
+    print(f"Epoka {epoch + 1}, strata: {loss:.4f}")
+
+# testowanie jedej próbki
+idx = 1
+x = x_test[idx].reshape(1, -1)
+y_true = y_test[idx]
+
+z1 = x @ w1 + b1
+a1 = relu(z1)
+z2 = a1 @ w2 + b2
+y_pred = softmax(z2)
+
+predicted_class = np.argmax(y_pred)
+
+print("Prawdziwa cyfra:", y_true)
+print("Sieć przewidziała: ", predicted_class)
+print("Poprawnie?", predicted_class == y_true)
+
+plt.imshow(x_test[idx].reshape(28, 28), cmap='gray')
+plt.title(f"Prawda: {y_true}, Przewidziano: {predicted_class}")
+plt.axis('off')
+plt.show()
